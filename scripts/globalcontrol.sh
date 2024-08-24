9da96014f00adcb882c1c1a0d89c6fb6cda6da59
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2024-08-25 01:36:24 +0600

    Add `generateWallpaperHash()` function - Generates hash for every wallpaper

--- scripts/globalcontrol.sh
+++ scripts/globalcontrol.sh
@@ -1,42 +1,42 @@
 #!/usr/bin/env bash
 
 export confDir="${XDG_CONFIG_HOME:-$HOME/.config}"
-export hydeConfDir="${confDir}/NyxDE"
+export nyxdeConfDir="${confDir}/NyxDE"
 
 export cacheDir="$HOME/.cache/NyxDE"
 export thumbDir="${cacheDir}/thumbs"
-export dcolDir="${cacheDir}/dcols"
+export dynamicColorDir="${cacheDir}/dcolors"
 
 export hashAlgorithm="sha1sum"
 
-get_hashmap() {
+generateWallpaperHash() {
     
-    unset wallpaperHash
-    unset wallpaperList
-    unset skipStrays
-    unset verboseMap
+    unset wallpaperHash         # Array for storing wallpaper Hash
+    unset wallpaperList         # Array for storing wallpaper names
+    unset verboseMode           # OPTIONAL - arg to set verbose mode
+    unset skipStrays            # OPTIONAL - 
     
-    for wallpaper_src in "$@"; do
-        [ -z "${wallpaper_src}" ] \
+    for wallpaperDir in "$@"; do
+        [ -z "${wallpaperDir}" ] \
         && continue
         
-        [ "${wallpaper_src}" == "--skipstrays" ] \
+        [ "${wallpaperDir}" == "--skipstrays" ] \
         && skipStrays=1 \
         && contine
         
-        [ "${wallpaper_src}" == "--verbose" ] \
-        && verboseMap=1 \
+        [ "${wallpaperDir}" == "--verbose" ] \
+        && verboseMode=1 \
         && continue
         
-        hashMap=$(find "${wallpaper_src}" -type f \( \
+        hashMap=$(find "${wallpaperDir}" -type f \( \
             -iname "*.gif"  -o \
             -iname "*.png"  -o \
             -iname "*.jpg"  -o \
-            -iname "*.jpeg" -o \
+            -iname "*.jpeg" \
         \) -exec "${hashAlgorithm}" {} + | sort -k2)
         
         if [ -z "${hashMap}" ]; then
-            echo "WARNING: No image found in \"${wallpaper_src}\""
+            echo "WARNING: No image found in \"${wallpaperDir}\""
             continue
         fi
         
@@ -46,7 +46,7 @@ get_hashmap() {
         done <<< "${hashMap}"
     done
 
-    if [ -z "${#wallpaperList[@]}" ] || [[ "${#wallpaperList[@]}" -eq 0 ]]; then
+    if [[ "${#wallpaperList[@]}" -eq 0 ]]; then
         if [[ "${skipStrays}" -eq 1 ]]; then
             return 1
         else
@@ -55,4 +55,15 @@ get_hashmap() {
         fi
     fi
     
+    if [[ "${verboseMode}" -eq 1 ]]; then
+        echo "------------------// Hash Map //------------------"
+        echo "Hash Algorithm  : ${hashAlgorithm}"
+        echo "Image Directory : ${wallpaperDir}"
+        echo "Images Processed: ${#wallpaperList[@]}"
+        echo "--------------------------------------------------"
+        for index in "${!wallpaperHash[@]}"; do
+            echo ":: \${wallpaperHash[$index]}=\"${wallpaperHash[$index]}\" :: \${wallpaperList[$index]=\"${wallpaperList[$index]}\""
+        done
+        echo "--------------------------------------------------"
+    fi
 }
