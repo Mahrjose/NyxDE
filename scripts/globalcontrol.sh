cd8074e6968096bba1caedb1710a567ad2bfecf8
Mirza Mahrab Hossain <mahrjose.dev@gmail.com>
2024-08-26 00:26:38 +0600

    Add `generateThemeList()`

--- scripts/globalcontrol.sh
+++ scripts/globalcontrol.sh
@@ -10,42 +10,50 @@ export dynamicColorDir="${cacheDir}/dcolors"
 export hashAlgorithm="sha1sum"
 
 generateWallpaperHash() {
-    
-    unset wallpaperHash         # Array for storing wallpaper Hash
-    unset wallpaperList         # Array for storing wallpaper names
-    unset verboseMode           # OPTIONAL - arg to set verbose mode
-    unset skipStrays            # OPTIONAL - 
-    
+
+    unset wallpaperHash # Array to store hashes of wallpapers
+    unset wallpaperList # Array to store file paths of wallpapers
+    unset verboseMode   # Flag to enable verbose output (default: off)
+    unset skipStrays    # Flag to skip processing directories with no images (default: off)
+
+    # Loop through all provided directories
     for wallpaperDir in "$@"; do
-        [ -z "${wallpaperDir}" ] \
-        && continue
-        
-        [ "${wallpaperDir}" == "--skipstrays" ] \
-        && skipStrays=1 \
-        && contine
-        
-        [ "${wallpaperDir}" == "--verbose" ] \
-        && verboseMode=1 \
-        && continue
-        
+
+        # Check if the directory path is empty or not provided
+        [ -z "${wallpaperDir}" ] &&
+            continue
+
+        # Check for special flags in arguments
+        [ "${wallpaperDir}" == "--skipstrays" ] &&
+            skipStrays=1 &&
+            continue
+
+        [ "${wallpaperDir}" == "--verbose" ] &&
+            verboseMode=1 &&
+            continue
+
+        # Generate a sorted list of file hashes and their paths
         hashMap=$(find "${wallpaperDir}" -type f \( \
-            -iname "*.gif"  -o \
-            -iname "*.png"  -o \
-            -iname "*.jpg"  -o \
+            -iname "*.gif" -o \
+            -iname "*.png" -o \
+            -iname "*.jpg" -o \
             -iname "*.jpeg" \
-        \) -exec "${hashAlgorithm}" {} + | sort -k2)
-        
+            \) -exec "${hashAlgorithm}" {} + | sort -k2)
+
+        # Check if no images were found and handle accordingly
         if [ -z "${hashMap}" ]; then
             echo "WARNING: No image found in \"${wallpaperDir}\""
             continue
         fi
-        
+
+        # Read each line from hashMap and populate arrays
         while read -r hash image; do
             wallpaperHash+=("${hash}")
             wallpaperList+=("${image}")
-        done <<< "${hashMap}"
+        done <<<"${hashMap}"
     done
 
+    # Check if no wallpapers were processed
     if [[ "${#wallpaperList[@]}" -eq 0 ]]; then
         if [[ "${skipStrays}" -eq 1 ]]; then
             return 1
@@ -54,7 +62,8 @@ generateWallpaperHash() {
             exit 1
         fi
     fi
-    
+
+    # Verbose output for debugging purposes
     if [[ "${verboseMode}" -eq 1 ]]; then
         echo "------------------// Hash Map //------------------"
         echo "Hash Algorithm  : ${hashAlgorithm}"
@@ -62,8 +71,61 @@ generateWallpaperHash() {
         echo "Images Processed: ${#wallpaperList[@]}"
         echo "--------------------------------------------------"
         for index in "${!wallpaperHash[@]}"; do
-            echo ":: \${wallpaperHash[$index]}=\"${wallpaperHash[$index]}\" :: \${wallpaperList[$index]=\"${wallpaperList[$index]}\""
+            echo ":: \${wallpaperHash[$index]}=\"${wallpaperHash[$index]}\" :: \${wallpaperList[$index]}=\"${wallpaperList[$index]}\""
         done
         echo "--------------------------------------------------"
+    fi
+}
+
+generateThemeList() {
+
+    unset themeOrderList # Array to store theme sort orders
+    unset themeList      # Array to store theme names
+    unset wallpaperList  # Array to store wallpaper paths
+
+    unset sortedThemeOrderList # Sorted array of theme sort orders
+    unset sortedThemeList      # Sorted array of theme names
+    unset sortedWallpaperList  # Sorted array of wallpaper paths
+
+    # Loop through each theme directory
+    while read -r themeDir; do
+
+        # Check if the symlink for wallpaper is missing or broken
+        if [ ! -e "$(readlink "${themeDir}/wallpaper.set")" ]; then
+
+            # Generate wallpaper hashes and possibly skip empty directories
+            generateWallpaperHash "${themeDir}" --skipstrays || continue
+
+            # Create or update the symlink to the current wallpaper
+            echo "Fixing wallpaper symlink :: ${themeDir}/wallpaper.set -> ${wallpaperList[0]}"
+            ln -fs "${wallpaperList[0]}" "${themeDir}/wallpaper.set"
+        fi
+
+        # Read the theme order from a file if it exists
+        if [ -f "${themeDir}/.themeOrder" ]; then
+            themeOrderList+=("$(head -1 "${themeDir}/.themeOrder")")
+        else
+            themeOrderList+=("0")
+        fi
+
+        # Add theme name and wallpaper path to lists
+        themeList+=("$(basename "${themeDir}")")
+        wallpaperList+=("$(readlink "${themeDir}/wallpaper.set")")
+
+    done < <(find "${nyxdeConfDir}/themes" -mindepth 1 -maxdepth 1 -type d)
+
+    # Combine the theme lists into a sorted list
+    while IFS='|' read -r order theme wallpaper; do
+        sortedThemeOrderList+=("${order}")
+        sortedThemeList+=("${theme}")
+        sortedWallpaperList+=("${wallpaper}")
+    done < <(parallel --link echo "{1}\|{2}\|{3}" ::: "${themeOrderList[@]}" ::: "${themeList[@]}" ::: "${wallpaperList[@]}" | sort -n -k 1 -k 2)
+
+    # Verbose output for debugging purposes
+    if [ "${1}" == "--verbose" ]; then
+        echo "// Theme Control //"
+        for index in "${!sortedThemeList[@]}"; do
+            echo -e ":: \${sortedThemeOrderList[${index}]}=\"${sortedThemeOrderList[index]}\" :: \${sortedThemeList[${index}]}=\"${sortedThemeList[index]}\" :: \${sortedWallpaperList[${index}]}=\"${sortedWallpaperList[index]}\""
+        done
     fi
 }
